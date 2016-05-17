eb0d57da4fbe81de8f7d0590ee2c9d7b2720480b
Brandon <broach@cern.ch>
2016-05-17 21:19:05 +0200

    changed grid_sub_many.sh to save file containing the iteration number which is then read by geant_fullsim_ecal_SPG_batch.py and sets the random seed

--- grid_sub_many.sh
+++ grid_sub_many.sh
@@ -2,21 +2,22 @@
 
 #copy the correct file to the detector area to prepare for run
 #also name the output dataset
-SM=4
-LAR=2.00
-LEAD=2.00
+LAR=4.24
+LEAD=2.44
 NTOT=1000
-E=500
-#cp new_ecal/${SM}mm/out_LAR${LAR}_LEAD${LEAD}.xml DetectorDescription/Detectors/compact/FCChh_ECalDefinition.xml
+E=20 #GeV
+#cp ../new_ecal/${SM}mm/out_LAR${LAR}_LEAD${LEAD}.xml DetectorDescription/Detectors/compact/FCChh_ECalDefinition.xml
 
 
 i=1
-TOT=11
-while [ $i -lt 11 ]
+TOT=1
+while [ $i -le $TOT ]
 do
-#prun --exec ". ./grid_test.sh" --cmtConfig=x86_64-slc6-gcc49-opt --site=CERN-PROD --outDS user.broach_e${E}_LAR${LAR}_LEAD${LEAD}_NTOT${NTOT}_BFIELD0_PART${i}_OF_${TOT} --outputs output.root
-echo $i
-echo user.broach_e${E}_LAR${LAR}_LEAD${LEAD}_NTOT${NTOT}_BFIELD0_PART${i}_OF_${TOT}
+echo $i >> iteration.txt
+prun --exec ". ./grid_test.sh" --cmtConfig=x86_64-slc6-gcc49-opt --site=CERN-PROD --outDS user.broach.e${E}_LAR${LAR}_LEAD${LEAD}_NTOT${NTOT}_BFIELD0_PART${i}_OF_${TOT} --outputs output.root
+#./run gaudirun.py geant_fullsim_ecal_SPG_batch.py
+echo user.broach.e${E}_LAR${LAR}_LEAD${LEAD}_NTOT${NTOT}_BFIELD0_PART${i}_OF_${TOT}
 i=$[$i+1]
+rm iteration.txt
 
 done
