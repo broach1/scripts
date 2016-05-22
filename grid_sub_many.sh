81ce92d16a9b7a105eba969caf79d1dbca169084
Brandon <broach@cern.ch>
2016-05-22 13:11:52 +0200

    Minor changes to grid_sub_many.sh to improve readability

--- grid_sub_many.sh
+++ grid_sub_many.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 
-i=1              #iteration number - begins with 1
-NJOBS=1          #total number of jobs to submit
-LAR=3.5          #mm
-LEAD=2.5         #mm
-SM=6             #LAR + LEAD
-NTOT=1000        #total number of events across all jobs: NTOT=EVTMAX*NJOBS
-BFIELD=0         #0 off, 1 on
-ENE=50e3         #MeV
-EVTMAX=100       #number of events in each job
-PHIMIN=0         #set phi range of generated particles
-PHIMAX=6.28      # ...
-VX=0             #set vertex X coordinate of generated particle (mm)
-VY=0             # Y ...
-VZ=0             # Z...
+i=1             
+#iteration number - begins with 1
 
-cp ../new_ecal_dim/${SM}mm/out_LAR${LAR}_LEAD${LEAD}.xml DetectorDescription/Detectors/compact/FCChh_ECalDefinition.xml
+NJOBS=25    
+#total number of jobs to submit
+
+LAR=3.5          
+LEAD=2.5         
+SM=6             
+#mm
+#SM=LAR+LEAD
+
+NTOT=500         
+#total number of events across all jobs: NTOT=EVTMAX*NJOBS
+
+BFIELD=0         
+#0 off, 1 on
+
+ENE=1000e3        
+#MeV
+
+EVTMAX=20        
+#number of events in each job
+
+PHIMIN=0    
+PHIMAX=6.28           
+#set phi range of generated particles
+
+VX=0          
+VY=0           
+VZ=0
+#set vertex coordinate of generated particle (mm)             
+
+#cp ../new_ecal_dim/${SM}mm/out_LAR${LAR}_LEAD${LEAD}.xml DetectorDescription/Detectors/compact/FCChh_ECalDefinition.xml
 
 
 while [ $i -le $NJOBS ]
@@ -30,11 +48,11 @@ sed -i "12s/.*/PHIMAX=$PHIMAX/g" geant_fullsim_ecal_SPG_batch.py
 sed -i "13s/.*/VX=$VX/g" geant_fullsim_ecal_SPG_batch.py 
 sed -i "14s/.*/VY=$VY/g" geant_fullsim_ecal_SPG_batch.py 
 sed -i "15s/.*/VZ=$VZ/g" geant_fullsim_ecal_SPG_batch.py 
-sed -i "16s/.*/i=$i/g" geant_fullsim_ecal_SPG_batch.py 
+sed -i "16s/.*/i=$i/g" geant_fullsim_ecal_SPG_batch.py
 
-#prun --exec ". ./grid_test.sh" --cmtConfig=x86_64-slc6-gcc49-opt --site=CERN-PROD --outDS user.broach.e${ENE}_LAR${LAR}_LEAD${LEAD}_NTOT${NTOT}_BFIELD${BFIELD}_PART${i}_OF_${NJOBS} --outputs output.root
-./run gaudirun.py geant_fullsim_ecal_SPG_batch.py
-echo user.broach.e${ENE}_LAR${LAR}_LEAD${LEAD}_NTOT${NTOT}_BFIELD${BFIELD}_PART${i}_OF_${NJOBS}
+prun --exec ". ./grid_test.sh" --cmtConfig=x86_64-slc6-gcc49-opt --site=CERN-PROD --outDS user.broach.e${ENE}_NO_CRYO_FIXED_LAR${LAR}_LEAD${LEAD}_NTOT${NTOT}_BFIELD${BFIELD}_VX${VX}_VY${VY}_VZ${VZ}_PART${i}_OF_${NJOBS} --outputs output.root
+#./run gaudirun.py geant_fullsim_ecal_SPG_batch.py
+echo user.broach.e${ENE}_NO_CRYO_FIXED_LAR${LAR}_LEAD${LEAD}_NTOT${NTOT}_BFIELD${BFIELD}_PART${i}_OF_${NJOBS}
 i=$[$i+1]
 
 done
